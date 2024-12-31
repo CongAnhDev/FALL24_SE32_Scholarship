d01733e09b06a93cac19bf91804e57fe603fd55a
vansang <sangnvde160492@fpt.edu.vn>
2024-12-31 22:22:02 +0700

    blog

---
+++ src/blog/blog.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import { BlogService } from './blog.service';
+import { BlogController } from './blog.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Blog, BlogSchema } from './schemas/blog.schemas';
+
+@Module({
+  imports: [
+    // Registers the Blog schema with Mongoose for database interactions
+    MongooseModule.forFeature([
+      { name: Blog.name, schema: BlogSchema },
+    ])
+  ],
+  controllers: [BlogController],
+  providers: [BlogService],
+})
+export class BlogModule { }
